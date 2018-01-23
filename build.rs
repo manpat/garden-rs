e04c33625005df927ad7ee829b5bd422fd7d7600
manpat <pat@patsown.com>
2018-01-23 20:18:49 +0000

    Updated index.html

--- build.rs
+++ build.rs
@@ -29,11 +29,18 @@ r##"<html>
 			html, body {
 				width: 100vw;
 				height: 100vh;
+				position: fixed;
 				overflow: hidden;
 				background: #69cc82;
 			}
 
 			canvas {
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+
 				overflow: hidden;
 				display: block;
 			}
